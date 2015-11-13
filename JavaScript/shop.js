d0976386a508d1e608450f0eec95bded82ef4b72
Darnell <darnelho1@gmail.com>
2015-11-13 13:57:10 -0800

    attempting to add method to do calculations

--- JavaScript/shop.js
+++ JavaScript/shop.js
@@ -1,12 +1,27 @@
 var donutShops = [];
+var hours = ['7:00am',
+             '8:00am',
+             '9:00am',
+             '10:00am',
+             '11:00am',
+             '12:00pm',
+             '1:00pm',
+             '2:00pm',
+             '3:00pm',
+             '4:00pm',
+             '5:00pm',
+             '6:00pm'];
 
-DonutShop=function(location,minCustHrs,maxCustHrs,avgDonutsPerCust){
+
+DonutShop=function(location,minCustHrs,maxCustHrs,avgDonutsPerCust,totalDonutsSld,donutsPerHr){
     this.location = location;
     this.minCustHrs = minCustHrs;
     this.maxCustHrs = maxCustHrs;
     this.avgDonutsPerCust = avgDonutsPerCust;
+    this.totalDonutsSld = '';
+    this.donutsPerHr=[];
         donutShops.push(this);
-}
+};
 
 var dwtnShop = new DonutShop('Downtown',8,43,4.5);
 var cHillShop = new DonutShop('Capitol Hill',4,37,2.00);
@@ -15,3 +30,24 @@ var wdgwoodShop = new DonutShop('Wedge Wood',2,28,1.25);
 var ballardShop = new DonutShop('Ballard',8,58,3.75);
 
 
+
+var test =[];
+
+
+
+ DonutShop.prototype.calculatedonutsPurchased = function(){
+  return hours.forEach(function(hour){
+      var calculate = (Math.floor(Math.random() * (this.maxCustHrs-this.minCustHrs + 1 )) + this.minCustHrs) * this.avgDonutsPerCust;
+      test = calculate;
+});
+};
+
+hour.forEach(function(hour){
+  return (Math.floor(Math.random() * (dwtnShop.maxCustHrs-dwtnShop.minCustHrs + 1)) + dwtnShop.minCustHrs) * dwtnShop.avgDonutsPerCust;
+  dwtnShop.donutsPerHr.push(hour)
+
+});
+
+var testDonutsSold = (Math.floor(Math.random() * (dwtnShop.maxCustHrs-dwtnShop.minCustHrs + 1)) + dwtnShop.minCustHrs) * dwtnShop.avgDonutsPerCust;
+
+var hour = (Math.floor(Math.random() * (cHillShop.maxCustHrs-this.minCustHrs + 1 )) + cHillShop.minCustHrs) * cHillShop.avgDonutsPerCust;
